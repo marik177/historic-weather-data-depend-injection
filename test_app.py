bd5a1e4a82a2c8fb0cbf3266473185e753d1e0aa
Roman K <marik177@gmail.com>
2023-09-03 12:50:50 +0300

    Initial commit, first test

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import datetime
+from pathlib import Path
+
+from app import App
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+
+def test_read():
+    app = App()
+    for key, value in app.read(file_name=Path(BASE_DIR).joinpath('london.csv')).items():
+        assert datetime.datetime.fromisoformat(key)
+        assert value - 0 == value
